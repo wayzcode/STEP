7c3506fe2c0896a152f78369ed6a42ad89c0ba86
Wayz <106933617+waymercy@users.noreply.github.com>
2026-03-26 12:02:39 +0100

    Update logout.php

--- horse/logout.php
+++ horse/logout.php
@@ -1,9 +1,11 @@
 <?php
+
+// Max: The logout page. Kills the user session and annihilates the cookie files where the session is stored.
+
 session_start();
 
 $_SESSION = array();
 
-// Annihilating cookies
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -13,6 +15,8 @@ if (ini_get("session.use_cookies")) {
 }
 
 session_destroy();
+
+// Max: After the process is done - send the user to index.php
 
 header("Location: index.php");
 
